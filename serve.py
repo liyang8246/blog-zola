2a15b98a86b642f1fddc02354d3aae3e909f612a
liyang <272891021@qq.com>
2024-12-10 16:11:22 +0800

    use npm

--- serve.py
+++ serve.py
@@ -4,7 +4,7 @@ def run_command(command):
     process = subprocess.Popen(command, shell=True)
     return process
 
-tailwind_process = run_command("tailwindcss -i ./static/styles/main.css -o ./static/styles/tailwind.css --watch")
+tailwind_process = run_command("npx tailwindcss -i ./static/styles/main.css -o ./static/styles/tailwind.css --watch")
 zola_process = run_command("zola serve")
 
 tailwind_process.wait()
